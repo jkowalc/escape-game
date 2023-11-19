2323eb21e4d00008520e43ea5a95dd6cf3917309
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-11-19 18:26:59 +0100

    List subplaces and cosmetic changes

--- prolog/rules/interaction.pl
+++ prolog/rules/interaction.pl
@@ -6,23 +6,40 @@
 
 examine(Thing) :-
    current_pos(Thing),
-   examination(Thing).
+   examination(Thing),
+   !, nl.
 
 examine(Thing) :-
    current_pos(CurrentPlace),
    subplace(CurrentPlace, Thing),
-   examination(Thing).
+   examination(Thing),
+   !, nl.
 
 examine(Thing) :-
    holding(Thing),
-   examination(Thing).
+   examination(Thing),
+   !, nl.
 
 examine(Thing) :-
    current_pos(CurrentPlace),
    pickable_item_at(CurrentPlace, Thing),
-   examination(Thing).
+   examination(Thing),
+   !, nl.
 
-examine(_) :-
+list_examine :-
+   current_pos(CurrentPlace),
+   \+ subplace(CurrentPlace, _),
+   write('You can examine: '), write(CurrentPlace),
+   !, nl.
+
+list_examine :-
+   current_pos(CurrentPlace),
+   write('You can examine: '), write(CurrentPlace), write(' '),
+   subplace(CurrentPlace, Place),
+   write(Place), write(' '),
+   fail.
+
+list_examine :-
    !, nl.
 
 examine :- 
