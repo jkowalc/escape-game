40e499eff6b573eef6d831eef3b18422df16d450
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-11-19 17:26:26 +0100

    Fix after rebase

--- prolog/map/usable_items.pl
+++ prolog/map/usable_items.pl
@@ -3,10 +3,6 @@
 ]).
 
 on_use(long_stick, nest) :-
-    write("You succesfully hit the CROW. Not only it's now away from his nest, but also he left something behind."),
-    (spawn_item(feather, window); spawn_item(screwdriver_bit, nest)).
-
-on_use(long_stick, nest) :-
     write("     I've succesfully pushed the RAVEN avay. Not only it's now away from his nest, but also he left something behind.
     ---NEW ITEM---"),
     retract(subplace(window, raven)),
