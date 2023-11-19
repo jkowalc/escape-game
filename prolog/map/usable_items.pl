f70db021997898cf302439f0f494de7d00cbb13c
Milan Wróblewski <milanw.2001@gmail.com>
2023-11-19 23:09:24 +0100

    fix on use long_stick

--- prolog/map/usable_items.pl
+++ prolog/map/usable_items.pl
@@ -15,8 +15,8 @@ on_use(long_stick, raven) :-
     write("     I've succesfully pushed the RAVEN avay. Not only it's now away from his nest, but also he left something behind.
     ---NEW ITEM---"),
     retract(subplace(window, raven)),
-    assert(subplace(window, nest)),
-    assert(pickable_item_at(feather, window)),
+    spawn_subplace(window, nest),
+    spawn_item(feather, window),
     % assert(pickable_item_at(screwdriver_bit, nest)),%done in examine nest
     !,nl.
 
