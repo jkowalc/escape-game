0f1df8375256575551b1b3fff167ef7d3a0f766f
Wiktor Topolski <wik.topolski@gmail.com>
2023-12-20 10:44:27 +0100

    try to locks

--- haskell/src/MapEventHandler/OnEnterCode.hs
+++ haskell/src/MapEventHandler/OnEnterCode.hs
@@ -1,1 +1,50 @@
 module MapEventHandler.OnEnterCode where
+import Data.Bool
+import Data.List (elemIndex)
+
+data LockState = LockState
+    {   
+        isOpen :: Bool
+        lockPassword :: String
+    }
+let LockNames = [10PadDigit, Vault, Computer, ColorCode]
+
+EnterCode :: GameState -> String -> String -> IO GameState
+
+EnterCode gs(inv, cp, lS) lockName user_code = 
+    let LockIndex = elemIndex lockName lS
+    case LockIndex of
+        Nothing -> printLines ""
+        _ -> do
+            if CheckIfReachable gs lockName == True then do
+                if user_code == (lockPassword lS !! LockIndex) then do
+                    lS = 
+                
+        
+    else
+
+EnterCode gs(inv, cp, lS) Vault user_code = do
+    pass
+
+EnterCode gs(inv, cp, lS) Computer user_code = do
+    pass
+
+EnterCode gs(inv, cp, lS) ColorCode user_code = do
+    pass    
+
+ChangeLockState :: GameState -> Integer -> Bool -> GameState
+ChangeLockState gs(inv, cp , lS) LockIndex newValue = do 
+    let newLockState = LockState newValue lockPassword (lS !! LockIndex)
+    let newlockStates = take index lS ++ newLockState ++ drop (index + 1) lS
+    GameState inv cp newlockStates
+
+
+CheckIfReachable :: GameState -> String -> Bool
+CheckIfReachable gs(inv, cp, lS) thingToReach = True
+
+NameIsALock :: String -> Bool
+NameIsALock ln = lockName elem LockNames
+
+CheckIfCorrectCode :: GameState -> String -> String -> Bool
+CheckIfCorrectCode gs(inv, cp, ls) lockName userCode = do
+    let Lock
--- haskell/src/State/GameState.hs
+++ haskell/src/State/GameState.hs
@@ -1,11 +1,12 @@
 module State.GameState where
 
-import Object.Item (Item)
+import Object.Item (Item, LockState)
 import Object.Place (Place (MainRoom))
 
 data GameState = GameState
     { inventory :: [Item]
     , currentPlace :: Place
+    , lockStates :: (LockState, LockState, LockState, LockState)    
     } deriving (Show)
 
 initialState :: GameState
