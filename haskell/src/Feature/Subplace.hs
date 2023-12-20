9d3c0723a926a57cc37e3410da27b401ee77f314
Milan Wróblewski <milanw.2001@gmail.com>
2023-12-20 11:58:39 +0100

    no duplicate subplaces

--- haskell/src/Feature/Subplace.hs
+++ haskell/src/Feature/Subplace.hs
@@ -5,9 +5,14 @@ import qualified Data.Map as Map
 
 
 spawnSubplace :: Place -> Place -> GameState -> IO GameState
-spawnSubplace place subplace state = do
-    putStrLn "You have discovered a new subplace!"
-    return state { unlockedSubplaces = Map.insertWith (++) place [subplace] (unlockedSubplaces state) }
+spawnSubplace place subplace state
+  | subplaceExists place subplace state = do
+      putStrLn "Nothing else to be found here"
+      return state
+  | otherwise = do
+      putStrLn "You have discovered a new subplace!"
+      return $ state { unlockedSubplaces = Map.insertWith (++) place [subplace] (unlockedSubplaces state) }
+
 
 despawnSubplace :: Place -> Place -> GameState -> GameState
 despawnSubplace place subplace state = state { unlockedSubplaces = Map.adjust (filter (/= subplace)) place (unlockedSubplaces state) }
